324bb5e1fc57fcbdcc5722e3738d22b63c5e2e2b
Rasika <info.rasika@gmail.com>
2015-05-28 16:07:19 +0530

    changing war name

---
+++ components/web-rest-api/src/main/java/org/wso2/carbon/device/mgt/iot/services/motorcar/MotorCarControllerService.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.device.mgt.iot.services.motorcar;
+
+import org.apache.log4j.Logger;
+import org.wso2.carbon.device.mgt.common.DeviceManagementException;
+import org.wso2.carbon.device.mgt.iot.config.FireAlarmConfigurationManager;
+import org.wso2.carbon.device.mgt.iot.config.FireAlarmManagementConfig;
+import org.wso2.carbon.device.mgt.iot.config.FireAlarmManagementControllerConfig;
+import org.wso2.carbon.device.mgt.iot.config.FireAlarmManagementSecurityConfig;
+import org.wso2.carbon.device.mgt.iot.config.controlqueue.FireAlarmControlQueueConfig;
+import org.wso2.carbon.device.mgt.iot.config.datastore.FireAlarmDataStoreConfig;
+import org.wso2.carbon.device.mgt.iot.devicecontroller.ControlQueueConnector;
+import org.wso2.carbon.device.mgt.iot.devicecontroller.DataStoreConnector;
+import org.wso2.carbon.device.mgt.iot.utils.ResourceFileLoader;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import java.io.File;
+import java.util.HashMap;
+
+@Path(value = "/DeviceController")
+public class MotorCarControllerService {
+
+	private static Logger log = Logger.getLogger(MotorCarControllerService.class);
+
+	private static DataStoreConnector iotDataStore = null;
+	private static ControlQueueConnector iotControlQueue = null;
+
+	static {
+
+		String trustStoreFile = null;
+		String trustStorePassword = null;
+		File certificateFile = null;
+
+		FireAlarmManagementConfig config = null;
+
+		try {
+			config = FireAlarmConfigurationManager.getInstance().getFireAlarmMgtConfig();
+		} catch (DeviceManagementException ex) {
+			log.error("Error occurred when trying to read configurations file: firealarm-config"
+							  + ".xml", ex);
+		}
+
+		if (config != null) {
+			/* reading security configurations */
+			FireAlarmManagementSecurityConfig securityConfig = config
+					.getFireAlarmManagementSecurityConfig();
+			trustStoreFile = securityConfig.getClient();
+			trustStorePassword = securityConfig.getTrustStorePassword();
+			certificateFile = new ResourceFileLoader("/resources/security/" + trustStoreFile)
+					.getFile();
+
+			if (certificateFile.exists()) {
+				trustStoreFile = certificateFile.getAbsolutePath();
+				log.info("Trust Store Path : " + trustStoreFile);
+
+				System.setProperty("javax.net.ssl.trustStore", trustStoreFile);
+				System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
+			} else {
+				log.error("Trust Store not found in path : " + trustStoreFile);
+			}
+
+			// controller configurations
+			FireAlarmManagementControllerConfig controllerConfig = config
+					.getFireAlarmManagementControllerConfig();
+
+			// reading data store configurations
+			String deviceDataStoreKey = controllerConfig.getDeviceDataStore();
+			FireAlarmDataStoreConfig dataStoreConfig = (FireAlarmDataStoreConfig) config
+					.getDataStoresMap().get(deviceDataStoreKey);
+			if (dataStoreConfig == null) {
+				log.error("Error occurred when trying to read data stores configurations");
+			}
+
+			//initialization data store
+			try {
+				String handlerClass = dataStoreConfig.getHandlerClass().trim();
+				Class<?> dataStore = MotorCarControllerService.class.forName(handlerClass);
+				if (DataStoreConnector.class.isAssignableFrom(dataStore)) {
+					iotDataStore = (DataStoreConnector) dataStore.newInstance();
+					iotDataStore.initDataStore();
+				}
+			} catch (ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
+				log.error("Error occurred when trying to initiate data store", ex);
+			}
+
+			// reading control queue configurations
+			String controlQueueKey = controllerConfig.getDeviceControlQueue();
+			FireAlarmControlQueueConfig controlQueueConfig = (FireAlarmControlQueueConfig) config
+					.getControlQueuesMap().get(controlQueueKey);
+			if (controlQueueConfig == null) {
+				log.error("Error occurred when trying to read control queue configurations");
+			}
+
+			//initialization control queue
+			try {
+				String handlerClass = controlQueueConfig.getHandlerClass().trim();
+				Class<?> controlQueue = MotorCarControllerService.class.forName(handlerClass);
+				if (ControlQueueConnector.class.isAssignableFrom(controlQueue)) {
+					iotControlQueue = (ControlQueueConnector) controlQueue.newInstance();
+					iotControlQueue.initControlQueue();
+				}
+			} catch (ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
+				log.error("Error occurred when trying to initiate control queue", ex);
+			}
+		}
+	}
+
+	@Path("/pushdata/{owner}/{type}/{id}/{time}/{key}/{value}")
+	@POST
+	// @Produces("application/xml")
+	public static String pushData(@PathParam("owner") String owner,
+								  @PathParam("type") String deviceType,
+								  @PathParam("id") String deviceId, @PathParam("time") Long time,
+								  @PathParam("key") String key, @PathParam("value") String value,
+								  @HeaderParam("description") String description,
+								  @Context HttpServletResponse response) {
+
+		HashMap<String, String> deviceDataMap = new HashMap<String, String>();
+
+		deviceDataMap.put("owner", owner);
+		deviceDataMap.put("deviceType", deviceType);
+		deviceDataMap.put("deviceId", deviceId);
+		deviceDataMap.put("time", "" + time);
+		deviceDataMap.put("key", key);
+		deviceDataMap.put("value", value);
+		deviceDataMap.put("description", description);
+
+		String result = "Failed to push";
+		result = iotDataStore.publishIoTData(deviceDataMap);
+		return result;
+	}
+
+	@Path("/setcontrol/{owner}/{type}/{id}/{key}/{value}")
+	@POST
+	public static String setControl(@PathParam("owner") String owner,
+									@PathParam("type") String deviceType,
+									@PathParam("id") String deviceId, @PathParam("key") String key,
+									@PathParam("value") String value) {
+		HashMap<String, String> deviceControlsMap = new HashMap<String, String>();
+
+		deviceControlsMap.put("owner", owner);
+		deviceControlsMap.put("deviceType", deviceType);
+		deviceControlsMap.put("deviceId", deviceId);
+		deviceControlsMap.put("key", key);
+		deviceControlsMap.put("value", value);
+
+		String result = null;
+		result = iotControlQueue.enqueueControls(deviceControlsMap);
+		return result;
+	}
+}
