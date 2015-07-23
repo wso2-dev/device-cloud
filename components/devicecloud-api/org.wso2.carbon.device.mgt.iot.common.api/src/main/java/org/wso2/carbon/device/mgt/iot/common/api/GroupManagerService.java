126d99420a9d62c8ea117616e135646b1c6d960c
charithag <charitha.ws@gmail.com>
2015-07-23 18:07:51 +0530

    Implement gorup management functionalities with JAX-RX

--- components/devicecloud-api/org.wso2.carbon.device.mgt.iot.common.api/src/main/java/org/wso2/carbon/device/mgt/iot/common/api/GroupManagerService.java
+++ components/devicecloud-api/org.wso2.carbon.device.mgt.iot.common.api/src/main/java/org/wso2/carbon/device/mgt/iot/common/api/GroupManagerService.java
@@ -1,25 +1,27 @@
 package org.wso2.carbon.device.mgt.iot.common.api;
 
+import org.wso2.carbon.device.mgt.common.Device;
 import org.wso2.carbon.device.mgt.group.common.Group;
 import org.wso2.carbon.device.mgt.group.common.GroupManagementException;
 import org.wso2.carbon.device.mgt.iot.common.GroupManagement;
+import org.wso2.carbon.user.core.Permission;
 
 import javax.ws.rs.*;
 import java.util.Date;
+import java.util.List;
 
 public class GroupManagerService {
 
     @Path("/group/add")
     @POST
     public boolean addGroup(@QueryParam("name") String name, @QueryParam("owner") String owner) {
-        GroupManagement groupManagement = new GroupManagement();
         Group group = new Group();
         group.setName(name);
         group.setOwnerId(owner);
         group.setDateOfCreation(new Date().getTime());
         group.setDateOfLastUpdate(new Date().getTime());
         try {
-            return groupManagement.createGroup(group);
+            return new GroupManagement().getGroupManagementService().createGroup(group);
         } catch (GroupManagementException e) {
             e.printStackTrace();
             return false;
@@ -29,14 +31,13 @@ public class GroupManagerService {
     @Path("/group/update/{groupId}")
     @POST
     public boolean updateGroup(@PathParam("groupId") int groupId, @QueryParam("name") String name, @QueryParam("owner") String owner) {
-        GroupManagement groupManagement = new GroupManagement();
         Group group = new Group();
         group.setId(groupId);
         group.setName(name);
         group.setOwnerId(owner);
         group.setDateOfLastUpdate(new Date().getTime());
         try {
-            groupManagement.updateGroup(group);
+            new GroupManagement().getGroupManagementService().updateGroup(group);
             return true;
         } catch (GroupManagementException e) {
             e.printStackTrace();
@@ -47,26 +48,156 @@ public class GroupManagerService {
     @Path("/group/delete/{groupId}")
     @DELETE
     public boolean deleteGroup(@PathParam("groupId") int groupId) {
-        GroupManagement groupManagement = new GroupManagement();
         try {
-            return groupManagement.deleteGroup(groupId);
+            return  new GroupManagement().getGroupManagementService().deleteGroup(groupId);
         } catch (GroupManagementException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    @Path("/group/get/{groupId}")
+    @Path("/group/id/{groupId}")
     @GET
     public Group getGroup(@PathParam("groupId") int groupId) {
-        GroupManagement groupManagement = new GroupManagement();
         try {
-            return groupManagement.getGroupById(groupId);
+            return  new GroupManagement().getGroupManagementService().getGroupById(groupId);
         } catch (GroupManagementException e) {
             e.printStackTrace();
             return null;
         }
     }
 
+    @Path("/group/name/{groupName}")
+    @GET
+    public Group getGroupByName(@PathParam("groupName") String groupName) {
+        try {
+            return  new GroupManagement().getGroupManagementService().getGroupByName(groupName);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Path("/group/all")
+    @GET
+    public Group[] getGroupsOfUser(@QueryParam("username") String username) {
+        try {
+            List<Group>groups =  new GroupManagement().getGroupManagementService().getGroupListOfUser(username);
+            Group[] groupArray = new Group[groups.size()];
+            return  groups.toArray(groupArray);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Path("/group/count")
+    @GET
+    public int getGroupCountOfUser(@QueryParam("username") String username) {
+        try {
+            return  new GroupManagement().getGroupManagementService().getGroupCountOfUser(username);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    @Path("/group/id/{groupId}/share")
+    @POST
+    public boolean shareGroup(@QueryParam("name") String username, @PathParam("groupId") int groupId, @QueryParam("role")String sharingRole) {
+        try {
+            return  new GroupManagement().getGroupManagementService().shareGroup(username, groupId, sharingRole);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Path("/group/id/{groupId}/unshare")
+    @POST
+    public boolean unShareGroup(@QueryParam("name") String username, @PathParam("groupId") int groupId, @QueryParam("role")String sharingRole) {
+        try {
+            return  new GroupManagement().getGroupManagementService().unShareGroup(username, groupId, sharingRole);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Path("/group/id/{groupId}/role/add")
+    @PUT
+    public boolean addNewSharingRoleForGroup(@QueryParam("name") String username, @PathParam("groupId") int groupId, @QueryParam("role") String roleName, @QueryParam("permissions") String[] permissions) {
+        try {
+            Permission[] perms = new Permission[permissions.length];
+            for (int i = 0; i < permissions.length; i++){
+                perms[i] = new Permission("group-mgt/user", permissions[i]);
+            }
+            return  new GroupManagement().getGroupManagementService().addNewSharingRoleForGroup(username, groupId, roleName, perms);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    @Path("/group/id/{groupId}/role/remove")
+    @DELETE
+    public boolean removeSharingRoleForGroup(@PathParam("groupId") int groupId, @QueryParam("role") String roleName) {
+        
+        try {
+            return  new GroupManagement().getGroupManagementService().removeSharingRoleForGroup(groupId, roleName);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Path("/group/id/{groupId}/role/all")
+    @GET
+    public String[] getAllRolesForGroup(@PathParam("groupId") int groupId) {
+        try {
+            List<String> roles =  new GroupManagement().getGroupManagementService().getAllRolesForGroup(groupId);
+            String[] rolesArray = new String[roles.size()];
+            return  roles.toArray(rolesArray);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Path("/group/id/{groupId}/role/all")
+    @GET
+    public String[] getGroupRolesForUser(@QueryParam("name") String username, @PathParam("groupId") int groupId) {
+        try {
+            List<String> roles =  new GroupManagement().getGroupManagementService().getGroupRolesForUser(username, groupId);
+            String[] rolesArray = new String[roles.size()];
+            return  roles.toArray(rolesArray);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Path("/group/id/{groupId}/user/all")
+    @GET
+    public String[] getUsersForGroup(@PathParam("groupId") int groupId) {
+        try {
+            return  new GroupManagement().getGroupManagementService().getUsersForGroup(groupId);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Path("/group/id/{groupId}/device/all")
+    @GET
+    public Device[] getAllDevicesInGroup(@PathParam("groupId") int groupId){
+        try {
+            List<Device> devices =  new GroupManagement().getGroupManagementService().getAllDevicesInGroup(groupId);
+            Device[] deviceArray = new Device[devices.size()];
+            return  devices.toArray(deviceArray);
+        } catch (GroupManagementException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
--- components/devicecloud-mgt/org.wso2.carbon.device.mgt.iot.common/src/main/java/org/wso2/carbon/device/mgt/iot/common/GroupManagement.java
+++ components/devicecloud-mgt/org.wso2.carbon.device.mgt.iot.common/src/main/java/org/wso2/carbon/device/mgt/iot/common/GroupManagement.java
@@ -19,112 +19,23 @@ package org.wso2.carbon.device.mgt.iot.common;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
-import org.wso2.carbon.device.mgt.common.Device;
-import org.wso2.carbon.device.mgt.common.DeviceIdentifier;
-import org.wso2.carbon.device.mgt.common.DeviceManagementException;
-import org.wso2.carbon.device.mgt.core.dao.DeviceManagementDAOException;
-import org.wso2.carbon.device.mgt.core.dao.DeviceManagementDAOFactory;
-import org.wso2.carbon.device.mgt.core.dto.DeviceType;
-import org.wso2.carbon.device.mgt.core.service.DeviceManagementProviderService;
-import org.wso2.carbon.device.mgt.group.common.Group;
-import org.wso2.carbon.device.mgt.group.common.GroupManagementException;
-import org.wso2.carbon.device.mgt.group.core.service.GroupManagementService;
-import org.wso2.carbon.device.mgt.group.core.service.GroupManagementServiceImpl;
-import org.wso2.carbon.device.mgt.iot.common.util.ZipArchive;
-import org.wso2.carbon.device.mgt.iot.common.util.iotdevice.util.IotDeviceManagementUtil;
-import org.wso2.carbon.user.core.Permission;
+import org.wso2.carbon.device.mgt.group.core.providers.GroupManagementServiceProvider;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-public class GroupManagement implements GroupManagementService{
+public class GroupManagement {
 
 	private static Log log = LogFactory.getLog(GroupManagement.class);
 
-    @Override
-    public boolean createGroup(Group group) throws GroupManagementException {
-        GroupManagementService groupManagementService = new GroupManagementServiceImpl();
-        return groupManagementService.createGroup(group);
-    }
+    public GroupManagementServiceProvider getGroupManagementService() {
 
-    public void updateGroup(Group group) throws GroupManagementException {
-        GroupManagementService groupManagementService = new GroupManagementServiceImpl();
-        groupManagementService.updateGroup(group);
-    }
-
-    @Override
-    public boolean deleteGroup(int groupId) throws GroupManagementException {
-        GroupManagementService groupManagementService = new GroupManagementServiceImpl();
-        return groupManagementService.deleteGroup(groupId);
-    }
-
-    public Group getGroupById(int groupId) throws GroupManagementException {
-        GroupManagementService groupManagementService = new GroupManagementServiceImpl();
-        return groupManagementService.getGroupById(groupId);
-    }
-
-    @Override
-    public Group getGroupByName(String groupName) throws GroupManagementException {
-        GroupManagementService groupManagementService = new GroupManagementServiceImpl();
-        return groupManagementService.getGroupByName(groupName);
-    }
-
-    @Override
-    public List<Group> getGroupListOfUser(String username) throws GroupManagementException {
-        GroupManagementService groupManagementService = new GroupManagementServiceImpl();
-        return groupManagementService.getGroupListOfUser(username);
-    }
-
-    @Override
-    public int getGroupCountOfUser(String s) throws GroupManagementException {
-        return 0;
-    }
-
-    @Override
-    public boolean shareGroup(String s, int i, String s1) throws GroupManagementException {
-        return false;
-    }
-
-    @Override
-    public boolean unShareGroup(String s, int i, String s1) throws GroupManagementException {
-        return false;
-    }
-
-    @Override
-    public boolean addNewSharingRoleForGroup(String s, int i, String s1, Permission[] permissions) throws GroupManagementException {
-        return false;
-    }
-
-    @Override
-    public boolean removeSharingRoleForGroup(int i, String s) throws GroupManagementException {
-        return false;
-    }
-
-    @Override
-    public List<String> getAllRolesForGroup(int i) throws GroupManagementException {
-        return null;
-    }
-
-    @Override
-    public List<String> getGroupRolesForUser(String s, int i) throws GroupManagementException {
-        return null;
-    }
-
-    @Override
-    public String[] getUsersForGroup(int i) throws GroupManagementException {
-        return new String[0];
-    }
-
-    @Override
-    public List<Device> getAllDevicesInGroup(int i) throws GroupManagementException {
-        return null;
-    }
-
-    @Override
-    public boolean addDeviceToGroup(DeviceIdentifier deviceIdentifier, int i) throws GroupManagementException {
-        return false;
+        GroupManagementServiceProvider groupManagementServiceProvider;
+        PrivilegedCarbonContext.startTenantFlow();
+        PrivilegedCarbonContext ctx = PrivilegedCarbonContext.getThreadLocalCarbonContext();
+        ctx.setTenantDomain(MultitenantConstants.SUPER_TENANT_DOMAIN_NAME);
+        ctx.setTenantId(MultitenantConstants.SUPER_TENANT_ID);
+        groupManagementServiceProvider =(GroupManagementServiceProvider) ctx.getOSGiService(GroupManagementServiceProvider.class, null);
+        PrivilegedCarbonContext.endTenantFlow();
+        return groupManagementServiceProvider;
     }
 
 }
