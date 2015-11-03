0c68afb1a8e35246f63056fb698e2f7e799d7ad4
Rasika Perera <info.rasika@gmail.com>
2015-11-03 11:13:02 +0530

    categorizing virtual devices

--- features/iotserver-feature/org.wso2.carbon.device.mgt.iot.feature/src/main/resources/jaggeryapps/store/extensions/assets/deviceType/themes/store/js/assets.js
+++ features/iotserver-feature/org.wso2.carbon.device.mgt.iot.feature/src/main/resources/jaggeryapps/store/extensions/assets/deviceType/themes/store/js/assets.js
@@ -1,8 +1,21 @@
-$('.ast-name.truncate').each(function(){
-    if($(this).attr('title') == 'virtual_firealarm'){
+var strVirtual = "virtual_";
+var foundVirtual = false;
+
+$('.real_devices .ast-name.truncate').each(function(){
+    if($(this).attr('title').substr(0, strVirtual.length) == 'virtual_'){
         var parent = $(this).closest('.ctrl-wr-asset');
         parent.hide();
-        $('.try-device-image').attr('src', parent.find('.img-responsive').attr('src'));
-        $('.try-device-text .btn').attr('href', parent.find('.ast-img ').attr('href'));
+        foundVirtual = true;
     }
 });
+
+$('.virtual_devices .ast-name.truncate').each(function(){
+    if($(this).attr('title').substr(0, strVirtual.length) != 'virtual_'){
+        var parent = $(this).closest('.ctrl-wr-asset');
+        parent.hide();
+    }
+});
+
+if(foundVirtual){
+    $(".try-device-message").removeClass("hidden");
+}
